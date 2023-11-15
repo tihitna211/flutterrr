e358fcb043c38b72b7b4d396263a433c40a3e619
tihitna211 <tihitnasol1@gmail.com>
2023-11-15 17:35:12 +0300

    profilee

--- lib/editprofilepage.dart
+++ lib/editprofilepage.dart
@@ -36,14 +36,14 @@ class _profileEditState extends State<profileEdit> {
                   Text('pick image'),
                   ListTile(
                     onTap: () {
-                      imagegal;
+                      imagegal();
                     },
                     leading: Icon(Icons.camera),
                     title: Text('from camera'),
                   ),
                   ListTile(
                     onTap: () {
-                      imagecam;
+                      imagecam();
                     },
                     leading: Icon(Icons.browse_gallery),
                     title: Text('from gallery'),
@@ -78,14 +78,17 @@ class _profileEditState extends State<profileEdit> {
                   CircleAvatar(
                     radius: 70,
                     child: (image != null)
-                        ? Image.file(File(image!.path))
+                        ? Image.file(
+                            File(image!.path),
+                            fit: BoxFit.cover,
+                          )
                         : Icon(
                             Icons.person_2,
                           ),
                   ),
                   Positioned(
-                    bottom: 20,
-                    right: 10,
+                    bottom: 10,
+                    right: 5,
                     child: IconButton(
                       icon: Icon(
                         Icons.camera_alt,
@@ -99,75 +102,81 @@ class _profileEditState extends State<profileEdit> {
               ),
             ),
           ),
-          Text(
-            'Name',
-            textAlign: TextAlign.left,
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          TextField(
-            decoration: InputDecoration(hintText: 'name'),
-          ),
-          Text(
-            'email',
-            textAlign: TextAlign.left,
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          TextField(
-            decoration: InputDecoration(hintText: 'email'),
-          ),
-          Text(
-            'password',
-            textAlign: TextAlign.left,
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          TextFormField(
-            obscureText: VisibleText,
-            onChanged: (value) {
-              if (value.length < 8) {
-                setState(() {
-                  error = "too short";
-                });
-              } else if (value.length >= 8 && value.length < 12) {
-                setState(() {
-                  error = "medium";
-                });
-              }
-              // ignore: curly_braces_in_flow_control_structures
-              else {
-                setState(() {
-                  error = "good!";
-                });
-              }
-              ;
-            },
-            decoration: InputDecoration(
-              errorText: error,
-              errorStyle: TextStyle(
-                  color: (error == 'good!')
-                      ? Colors.green
-                      : (error == "too short")
-                          ? Colors.red
-                          : (error == 'medium')
-                              ? Colors.amber
-                              : null),
-              suffixIcon: IconButton(
-                onPressed: () {
-                  setState(() {
-                    VisibleText = !VisibleText;
-                  });
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Name',
+                textAlign: TextAlign.left,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              TextField(
+                decoration: InputDecoration(hintText: 'name'),
+              ),
+              Text(
+                'email',
+                textAlign: TextAlign.left,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              TextField(
+                decoration: InputDecoration(hintText: 'email'),
+              ),
+              Text(
+                'password',
+                textAlign: TextAlign.left,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                obscureText: VisibleText,
+                onChanged: (value) {
+                  if (value.length < 8) {
+                    setState(() {
+                      error = "too short";
+                    });
+                  } else if (value.length >= 8 && value.length < 12) {
+                    setState(() {
+                      error = "medium";
+                    });
+                  }
+                  // ignore: curly_braces_in_flow_control_structures
+                  else {
+                    setState(() {
+                      error = "good!";
+                    });
+                  }
+                  ;
                 },
-                icon: Icon((VisibleText)
-                    ? Icons.visibility
-                    : (!VisibleText)
-                        ? Icons.visibility_off_outlined
-                        : null),
+                decoration: InputDecoration(
+                  errorText: error,
+                  errorStyle: TextStyle(
+                      color: (error == 'good!')
+                          ? Colors.green
+                          : (error == "too short")
+                              ? Colors.red
+                              : (error == 'medium')
+                                  ? Colors.amber
+                                  : null),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        VisibleText = !VisibleText;
+                      });
+                    },
+                    icon: Icon((VisibleText)
+                        ? Icons.visibility
+                        : (!VisibleText)
+                            ? Icons.visibility_off_outlined
+                            : null),
+                  ),
+                  hintText: "password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
               ),
-              hintText: "password",
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
-          ),
+            ],
+          )
         ],
       )), //positioned(icon,bottom, left)
     );
